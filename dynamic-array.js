d98724e9313afacc8e343a0d3bda4267acf50504
Ryan <ryanjerickson@gmail.com>
2023-04-18 16:41:42 -0500

    First push

--- dynamic-array.js
+++ dynamic-array.js
@@ -20,19 +20,38 @@ class DynamicArray {
 
 
   pop() {
+    if (this.length === 0) {
+      return undefined
+    }
+    const last = this.data[this.length - 1]
+    this.data[this.length - 1] = undefined;
+    this.length--
+    return last;
 
-    // Your code here
   }
 
   shift() {
+    if (this.length === 0) {
+      return undefined
+    }
+    const first = this.data[0];
+    for (let i = 0; i < this.length; i++) {
+      this.data[i] = this.data[i + 1]
+    }
+    this.data[this.length - 1] = undefined;
+    this.length--
+    return first;
 
-    // Your code here
   }
 
   unshift(val) {
-
-    // Your code here
+    for (let i = this.length; i >= 0; i--) {
+      this.data[i] = this.data[i - 1]
+    }
+    this.data[0] = val;
+    this.length++
   }
+
 
   indexOf(val) {
 
